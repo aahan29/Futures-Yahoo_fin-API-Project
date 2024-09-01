b8baf2242a53abf8b8da7facf3401868e196e589
Aahan Madhok <151942445+aahan29@users.noreply.github.com>
2024-09-01 19:05:50 -0400

    Add files via upload

---
+++ Futures.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import yahoo_fin.stock_info as si
+import pandas as pd
+
+# general futures ticker // could be continuous?
+ticker = "GCU1"
+                   
+# specific contract ticker                   
+# ticker = "AAPL"
+
+# request data 
+data = si.get_data(ticker, start_date = "01/01/1900",
+                   end_date = "01/01/2026")
+
+# access the data
+closingPrices = data.adjclose
+
+# quick plot
+data.adjclose.plot()
+
+
